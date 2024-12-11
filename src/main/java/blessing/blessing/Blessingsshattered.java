240eea6f1eb1052126ae88e0036d237144f6f85e
valor <isaacisapop@gmail.com>
2024-12-10 23:06:29 -0500

    Initial commit

---
+++ src/main/java/blessing/blessing/Blessingsshattered.java
@@ -0,0 +1,22 @@
+package blessing.blessing;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import blessing.blessing.item.ModItems;
+
+public class Blessingsshattered implements ModInitializer {
+	public static final String MOD_ID = "blessingsshattered";
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		ModItems.registerModItems();
+		LOGGER.info("initializing Blessings Shattered");
+	}
+}
---
+++ src/main/java/blessing/blessing/item/ModItems.java
@@ -0,0 +1,35 @@
+package blessing.blessing.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import blessing.blessing.Blessingsshattered;
+
+public class ModItems {
+
+
+    //
+    public static final Item blessing_shard = registerItem("blessing_shard", new Item(new FabricItemSettings().maxCount(16)));
+
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(Blessingsshattered.MOD_ID, name), item);
+    }
+
+    public static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries){
+        entries.add(blessing_shard);
+    }
+
+
+    public static void registerModItems() {
+        Blessingsshattered.LOGGER.info("Registering Mod Items for " + Blessingsshattered.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientItemGroup);
+    }
+}
